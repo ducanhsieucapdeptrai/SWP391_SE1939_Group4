6d2a7c4e1d9a1e44233ce571c1562f2fb345c662
Phuoc-PewPew99 <phuocnhhe181787@fpt.edu.vn>
2025-07-12 10:16:55 +0700

    Update WarehouseReportServlet.java lần 2

--- Quanlyvattu/src/java/controller/general/WarehouseReportServlet.java
+++ Quanlyvattu/src/java/controller/general/WarehouseReportServlet.java
@@ -17,6 +17,7 @@ import java.util.Map;
 
 @WebServlet("/warehousereport")
 public class WarehouseReportServlet extends HttpServlet {
+
     private WarehouseReportDAO warehouseReportDAO;
 
     @Override
@@ -27,19 +28,20 @@ public class WarehouseReportServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         int requestId = Integer.parseInt(request.getParameter("requestId"));
-        
+
         // Get data from DAO
         RequestList requestList = warehouseReportDAO.getRequestById(requestId);
         List<RequestDetailItem> requestDetails = warehouseReportDAO.getRequestDetailsById(requestId);
         Map<Date, List<TaskLog>> taskLogsByDate = warehouseReportDAO.getTaskLogsByDate(requestId);
-
+        int totalSlips = taskLogsByDate.values().stream().mapToInt(List::size).sum();
         // Set attributes for JSP
+        request.setAttribute("totalSlips", totalSlips);
         request.setAttribute("request", requestList);
         request.setAttribute("requestDetails", requestDetails);
         request.setAttribute("taskLogsByDate", taskLogsByDate);
 
         // Forward to JSP
-         request.setAttribute("pageContent", "/WarehouseStaffReport.jsp");
-                request.getRequestDispatcher("/layout/layout.jsp").forward(request, response);
+        request.setAttribute("pageContent", "/WarehouseStaffReport.jsp");
+        request.getRequestDispatcher("/layout/layout.jsp").forward(request, response);
     }
 }
